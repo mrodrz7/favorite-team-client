3e506ceeb6273424e2a01398c9809d8542c4b3a6
mrodrz7 <m.rodriguez0743@gmail.com>
2019-10-18 09:17:20 -0400

    Add links to Header
    - Add 'See all Teams' link to header.js
    - Add 'Add a Team' link to header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,8 @@ const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <Nav.Link href="#teams">See all Teams</Nav.Link>
+    <Nav.Link href="#create-book">Add a Team</Nav.Link>
   </Fragment>
 )
 
